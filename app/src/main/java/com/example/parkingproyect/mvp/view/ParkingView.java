b549132779e10337b4960d34e90d3eb39337fa78
Bruno Rodriguez <bruno.rodriguez@globant.com>
2021-06-18 14:21:36 -0300

    Solved the problem in ParkingView->showSizeMessage

--- app/src/main/java/com/example/parkingproyect/mvp/view/ParkingView.java
+++ app/src/main/java/com/example/parkingproyect/mvp/view/ParkingView.java
@@ -3,6 +3,7 @@ package com.example.parkingproyect.mvp.view;
 import android.app.Activity;
 import android.widget.Toast;
 
+import com.example.parkingproyect.R;
 import com.example.parkingproyect.databinding.ActivityMainBinding;
 
 public class ParkingView extends ActivityView {
@@ -20,11 +21,7 @@ public class ParkingView extends ActivityView {
     }
 
     public void showSizeMessage(String size){
-        //Sorry for the big comment :(
-        //I dont now why but R.string.text_parking_add_size_message throw me a number
-        //string.xml have this <string name="text_parking_add_size_message">Parking lots added: </string>
-        //String text = R.string.text_parking_add_size_message + " " + size;
-        //binding.textParkingMessage.setText(text);
+        binding.textParkingMessage.setText(getContext().getString(R.string.text_parking_add_size_message, size));
         binding.textParkingMessage.setText("Parking lots added: " + size);
     }
 
