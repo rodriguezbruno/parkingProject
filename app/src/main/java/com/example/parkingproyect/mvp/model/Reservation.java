3c815f23d547e35df275116cc95ac936d9d48d59
Bruno Rodriguez <bruno.rodriguez@globant.com>
2021-06-21 16:19:14 -0300

    Models to start the functionality

---
+++ app/src/main/java/com/example/parkingproyect/mvp/model/Reservation.java
@@ -0,0 +1,52 @@
+package com.example.parkingproyect.mvp.model;
+
+import java.util.Date;
+
+public class Reservation {
+
+    public int idReservation;
+    public ParkingLot parkingLot;
+    public Date startDateTime;
+    public Date finishDateTime;
+    public String securityCode;
+
+    public int getIdReservation() {
+        return idReservation;
+    }
+
+    public void setIdReservation(int idReservation) {
+        this.idReservation = idReservation;
+    }
+
+    public ParkingLot getParkingLot() {
+        return parkingLot;
+    }
+
+    public void setParkingLot(ParkingLot parkingLot) {
+        this.parkingLot = parkingLot;
+    }
+
+    public Date getStartDateTime() {
+        return startDateTime;
+    }
+
+    public void setStartDateTime(Date startDateTime) {
+        this.startDateTime = startDateTime;
+    }
+
+    public Date getFinishDateTime() {
+        return finishDateTime;
+    }
+
+    public void setFinishDateTime(Date finishDateTime) {
+        this.finishDateTime = finishDateTime;
+    }
+
+    public String getSecurityCode() {
+        return securityCode;
+    }
+
+    public void setSecurityCode(String securityCode) {
+        this.securityCode = securityCode;
+    }
+}
